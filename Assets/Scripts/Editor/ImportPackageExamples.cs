7b2e0b92ce662bf4354df0300454526ee625d949
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2024-03-11 22:08:51 +0100

    ~ Implemented a script to update the Samples folder in the Assets for the RGN packages.

---
+++ Assets/Scripts/Editor/ImportPackageExamples.cs
@@ -0,0 +1,69 @@
+using Newtonsoft.Json.Linq;
+using System.IO;
+using UnityEditor;
+
+public class ImportPackageSamples
+{
+    [MenuItem("Tools/Import Package Examples")]
+    public static void ImportPackageExamples()
+    {
+        string samplesPath = "Assets/Samples";
+        if (Directory.Exists(samplesPath))
+        {
+            Directory.Delete(samplesPath, true);
+        }
+        Directory.CreateDirectory(samplesPath);
+
+        string packageCachePath = Path.Combine(Directory.GetCurrentDirectory(), "Library/PackageCache");
+        string[] packageDirectories = Directory.GetDirectories(packageCachePath, "io.getready.rgn*", SearchOption.AllDirectories);
+
+        foreach (string packageDir in packageDirectories)
+        {
+            string packageJsonPath = Path.Combine(packageDir, "package.json");
+            if (!File.Exists(packageJsonPath))
+            {
+                continue;
+            }
+            string packageJsonContent = File.ReadAllText(packageJsonPath);
+            JObject packageJson = JObject.Parse(packageJsonContent);
+
+            if (packageJson["samples"] == null)
+            {
+                continue;
+            }
+            JArray samples = (JArray)packageJson["samples"];
+            if (samples.Count == 0)
+            {
+                continue;
+            }
+            string displayName = packageJson["displayName"].ToString().Replace("/", "_");
+            string version = packageJson["version"].ToString();
+            string sampleRootPath = Path.Combine(samplesPath, displayName, version);
+            Directory.CreateDirectory(sampleRootPath);
+            foreach (JObject sample in samples)
+            {
+                string sampleName = sample["displayName"].ToString().Replace("/", "_");
+                string samplePath = sample["path"].ToString();
+                string sampleFullPath = Path.Combine(packageDir, samplePath);
+                string destinationPath = Path.Combine(sampleRootPath, sampleName);
+                CopyDirectory(sampleFullPath, destinationPath);
+            }
+        }
+        AssetDatabase.Refresh();
+    }
+
+    private static void CopyDirectory(string sourceDir, string destinationDir)
+    {
+        Directory.CreateDirectory(destinationDir);
+        foreach (var file in Directory.GetFiles(sourceDir))
+        {
+            string destFile = Path.Combine(destinationDir, Path.GetFileName(file));
+            File.Copy(file, destFile);
+        }
+        foreach (var dir in Directory.GetDirectories(sourceDir))
+        {
+            string destDir = Path.Combine(destinationDir, Path.GetFileName(dir));
+            CopyDirectory(dir, destDir);
+        }
+    }
+}
