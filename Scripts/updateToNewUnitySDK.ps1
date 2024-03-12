b5953128d12d6551248bae029856cad7794b880a
Konstantin Gindemit <konstantin.gindemit@gmail.com>
2024-03-12 10:11:17 +0100

    ~ Using a regex that can parse the release and dev versions

--- Scripts/updateToNewUnitySDK.ps1
+++ Scripts/updateToNewUnitySDK.ps1
@@ -7,11 +7,11 @@ $ErrorActionPreference = 'Stop'
 $scriptLocation = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $sdkRepository = $scriptLocation + "/.."
 
-Function UpdateVersion($newVersion, $regexToParseTheVersion) {
+Function UpdateVersion($newVersion) {
   $currentBranch = git -C "$sdkRepository" rev-parse --abbrev-ref HEAD
   $manifestJsonFilePath = "$sdkRepository/Packages/manifest.json"
   $manifestJsonContent = Get-Content -Path $manifestJsonFilePath -Raw
-  $oldVersion = [regex]::Match($manifestJsonContent, """io\.getready\.rgn\.core"": ""https://github\.com/readyio/RGNCore\.git#($regexToParseTheVersion)").Groups[1].Value
+  $oldVersion = [regex]::Match($manifestJsonContent, """io\.getready\.rgn\.core"": ""https://github\.com/readyio/RGNCore\.git#(\d+\.\d+\.\d+(-[0-9A-Za-z-]+(\.\d+)*)?)""").Groups[1].Value
   Write-Host "Old version: $oldVersion"
   $updatedManifestJsonContent = $manifestJsonContent -replace $oldVersion, $newVersion
   Set-Content -Path $manifestJsonFilePath -Value $updatedManifestJsonContent -Encoding UTF8 -NoNewline
@@ -55,4 +55,4 @@ git -C "$sdkRepository" checkout .
 git -C "$sdkRepository" clean -fd
 git -C "$sdkRepository" pull
 
-UpdateVersion $version "\d+\.\d+\.\d+"
+UpdateVersion $version
